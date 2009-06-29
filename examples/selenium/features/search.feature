d7c757b587c741dd10dfb901cf2906961d9d6bba
Roman Lisagor <rlisagor@gmail.com>
2009-06-29 15:37:54 -0700

    Factored out selenium into a "web" module

--- examples/selenium/features/search.feature
+++ examples/selenium/features/search.feature
@@ -3,6 +3,7 @@ Feature: Search
   As an information seeker
   I want to find more information
 
+  @selenium
   Scenario: Find what I'm looking for
     Given I am on the Google search page
     When I search for "rspec"
--- examples/selenium/features/steps/__init__.py
+++ examples/selenium/features/steps/__init__.py
@@ -1,33 +1,5 @@
 from search_steps import *
-from freshen import *
-from freshen import selenium
-import atexit
+from freshen import web
 
-import logging
-log = logging.getLogger('nose.plugins')
-
-# "before all"
-log.debug("Setting up selenium")
-glc.browser = selenium.selenium("localhost", 4444, "*firefox", "http://localhost")
-
-@Before
-def start_browser(sc):
-    log.debug("Starting selenium")
-    glc.browser.start()
-
-@After
-def stop_browser(sc):
-    log.debug("Stopping selenium")
-    glc.browser.stop()
-
-# "after all"
-@atexit.register
-def close_browser():
-    log.debug("Closing selenium")
-    try:
-        glc.browser.close()
-    except:
-        pass
-
-
+web.use_selenium(tags=["selenium"])
 
