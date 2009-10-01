61bfe7ead92eca4bf43b1a4a3fd91b7e3028e0ad
Bruno Bord <bruno@jehaisleprintemps.net>
2009-10-01 22:36:32 +0200

    setup now with language.yml integrated

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ from setuptools import setup
 setup(
     name='Freshen plugin',
     install_requires=['pyparsing>=1.5.0'],
-    version='0.1',
+    version='0.1-bb',
     author='Roman Lisagor',
     author_email = 'rlisagor+freshen@gmail.com',
     description = 'Freshen - clone of the Cucumber BDD framework',
     license = 'GNU LGPL',
     py_modules = ['freshen'],
     packages = ['freshen'],
+    data_files = [('freshen', ['freshen/languages.yml'])],
     entry_points = {
         'nose.plugins.0.10': [
             'freshen = freshen:FreshenNosePlugin',
