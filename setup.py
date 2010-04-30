1374d5732b845bb21050a376e89c8754a7f4834e
Roman Lisagor <rlisagor@gmail.com>
2010-04-30 11:51:40 -0700

    Updated setup.py and todo.txt for release 0.2

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ from setuptools import setup
 setup(
     name='Freshen plugin',
     install_requires=['pyparsing>=1.5.0', 'PyYAML'],
-    version='0.1',
+    version='0.2',
     author='Roman Lisagor',
     author_email = 'rlisagor+freshen@gmail.com',
     description = 'Freshen - clone of the Cucumber BDD framework',
-    license = 'GNU LGPL',
+    license = 'GNU GPL',
     py_modules = ['freshen'],
     packages = ['freshen'],
     data_files = [('freshen', ['freshen/languages.yml'])],
