c900a8afd85cb88c31e3f246a4e6d99bd24c6617
Roman Lisagor <roman@demonware.net>
2011-01-11 13:14:40 -0800

    Bug fix: named argument transform test didn't support python2.5

--- examples/befriending_many/features/steps.py
+++ examples/befriending_many/features/steps.py
@@ -1,6 +1,26 @@
 from freshen import *
 from friends import find_user
-from itertools import combinations
+
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+
 
 @Transform(r'^user (\w+)$')
 def transform_user(username):
