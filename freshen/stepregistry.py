c49d67a74bdfa28866232cd18a00664d3f071a3c
jerico.dev <jerico.dev@gmail.com>
2011-06-03 22:17:45 -0300

    fix trailing whitespace and other whitespace issues (see PEP8)

--- freshen/stepregistry.py
+++ freshen/stepregistry.py
@@ -13,7 +13,7 @@ __unittest = 1
 log = logging.getLogger('freshen')
 
 class AmbiguousStepImpl(Exception):
-    
+
     def __init__(self, step, impl1, impl2):
         self.step = step
         self.impl1 = impl1
@@ -23,26 +23,26 @@ class AmbiguousStepImpl(Exception):
                                                                               impl2.get_location()))
 
 class UndefinedStepImpl(Exception):
-    
+
     def __init__(self, step):
         self.step = step
         super(UndefinedStepImpl, self).__init__('"%s" # %s' % (step.match, step.source_location()))
 
 class StepImpl(object):
-    
+
     def __init__(self, step_type, spec, func):
         self.step_type = step_type
         self.spec = spec
         self.func = func
         self.named_transforms = []
 
-    def apply_named_transform( self, name, pattern, transform ):
+    def apply_named_transform(self, name, pattern, transform):
         if name in self.spec:
-            self.spec = self.spec.replace( name, pattern )
-            self.named_transforms.append( transform )
-            if hasattr( self, 're_spec' ):
+            self.spec = self.spec.replace(name, pattern)
+            self.named_transforms.append(transform)
+            if hasattr(self, 're_spec'):
                 del self.re_spec
-    
+
     def run(self, *args, **kwargs):
         self.func(*args, **kwargs)
 
@@ -50,62 +50,62 @@ class StepImpl(object):
         self.func(*args, **kwargs)
     
     def match(self, match):
-        if not hasattr( self, 're_spec' ):
-            self.re_spec = re.compile( self.spec )
+        if not hasattr(self, 're_spec'):
+            self.re_spec = re.compile(self.spec)
         return self.re_spec.match(match)
-        
+
     def get_location(self):
         code = self.func.func_code
         return "%s:%d" % (code.co_filename, code.co_firstlineno)
 
 class HookImpl(object):
-    
+
     def __init__(self, cb_type, func, tags=[]):
         self.cb_type = cb_type
         self.tags = tags
         self.func = func
         self.tags = tags
         self.order = 0
-    
+
     def __repr__(self):
         return "<Hook: @%s %s(...)>" % (self.cb_type, self.func.func_name)
-    
+
     def run(self, scenario):
         self.func(scenario)
-    
+
     def __call__(self, *args, **kwargs):
         self.func(*args, **kwargs)
 
 class TransformImpl(object):
-    
+
     def __init__(self, spec_fragment, func):
         self.spec_fragment = spec_fragment
         self.re_spec = re.compile(spec_fragment)
         self.func = func
-    
+
     def is_match(self, arg):
         if arg is None:
             return False
         return self.re_spec.match(arg) != None
-    
+
     def transform_arg(self, arg):
         match = self.re_spec.match(arg)
         if match:
             return self.func(*match.groups())
-    
+
     def __call__(self, *args, **kwargs):
         self.func(*args, **kwargs)
 
-class NamedTransformImpl( TransformImpl ):
+class NamedTransformImpl(TransformImpl):
 
-    def __init__( self, name, in_pattern, out_pattern, func ):
-        super( NamedTransformImpl, self ).__init__( out_pattern, func )
+    def __init__(self, name, in_pattern, out_pattern, func):
+        super(NamedTransformImpl, self).__init__(out_pattern, func)
         self.name = name
         self.in_pattern = in_pattern
         self.out_pattern = out_pattern
 
-    def apply_to_step( self, step ):
-        step.apply_named_transform( self.name, self.in_pattern, self )
+    def apply_to_step(self, step):
+        step.apply_named_transform(self.name, self.in_pattern, self)
 
 
 class StepImplLoadException(Exception):
@@ -118,27 +118,27 @@ class StepImplLoader(object):
     def __init__(self):
         self.modules = {}
         self.module_counter = 0
-    
+
     def load_steps_impl(self, registry, path, module_names=None):
         """
         Load the step implementations at the given path, with the given module names. If
         module_names is None then the module 'steps' is searched by default.
         """
-        
+
         if not module_names:
             module_names = ['steps']
-        
+
         path = os.path.abspath(path)
-        
+
         for module_name in module_names:
             mod = self.modules.get((path, module_name))
-            
+
             if mod is None:
                 #log.debug("Looking for step def module '%s' in %s" % (module_name, path))
                 cwd = os.getcwd()
                 if cwd not in sys.path:
                     sys.path.append(cwd)
-                
+
                 try:
                     actual_module_name = os.path.basename(module_name)
                     complete_path = os.path.join(path, os.path.dirname(module_name))
@@ -153,10 +153,10 @@ class StepImplLoader(object):
                 except:
                     exc = sys.exc_info()
                     raise StepImplLoadException(exc)
-                
+
                 self.module_counter += 1
                 self.modules[(path, module_name)] = mod
-            
+
             for item_name in dir(mod):
                 item = getattr(mod, item_name)
                 if isinstance(item, StepImpl):
@@ -169,46 +169,46 @@ class StepImplLoader(object):
                     registry.add_transform(item)
 
 class StepImplRegistry(object):
-    
+
     def __init__(self, tag_matcher_class):
         self.steps = {
             'given': [],
             'when': [],
             'then': []
         }
-        
+
         self.hooks = {
             'before': [],
             'after': [],
             'after_step': []
         }
-        
+
         self.transforms = []
         self.named_transforms = []
         self.tag_matcher_class = tag_matcher_class
-    
+
     def add_step(self, step_type, step):
         self.steps[step_type].append(step)
         for named_transform in self.named_transforms:
-            named_transform.apply_to_step( step )
-    
+            named_transform.apply_to_step(step)
+
     def add_hook(self, hook_type, hook):
         self.hooks[hook_type].append(hook)
-    
+
     def add_transform(self, transform):
         self.transforms.append(transform)
 
-    def add_named_transform( self, named_transform ):
-        self.named_transforms.append( named_transform )
-        for step in chain( *self.steps.values() ):
-            named_transform.apply_to_step( step )
-    
+    def add_named_transform(self, named_transform):
+        self.named_transforms.append(named_transform)
+        for step in chain(*self.steps.values()):
+            named_transform.apply_to_step(step)
+
     def _apply_transforms(self, arg, step):
-        for transform in chain( step.named_transforms, self.transforms ):
+        for transform in chain(step.named_transforms, self.transforms):
             if transform.is_match(arg):
                 return transform.transform_arg(arg)
         return arg
-    
+
     def find_step_impl(self, step):
         """
         Find the implementation of the step for the given match string. Returns the StepImpl object
@@ -225,17 +225,17 @@ class StepImplRegistry(object):
             if matches:
                 if result:
                     raise AmbiguousStepImpl(step, result[0], si)
-                
+
                 args = [self._apply_transforms(arg, si) for arg in matches.groups()]
                 result = si, args
-        
+
         if not result:
             raise UndefinedStepImpl(step)
         return result
-    
+
     def get_hooks(self, cb_type, tags=[]):
         hooks = [h for h in self.hooks[cb_type] if self.tag_matcher_class(h.tags).check_match(tags)]
-        hooks.sort(cmp=lambda x,y: cmp(x.order, y.order))
+        hooks.sort(cmp=lambda x, y: cmp(x.order, y.order))
         return hooks
 
 
@@ -267,10 +267,10 @@ def transform_decorator(spec_fragment):
         return TransformImpl(spec_fragment, func)
     return wrapper
 
-def named_transform_decorator( name, in_pattern, out_pattern = None ):
+def named_transform_decorator(name, in_pattern, out_pattern=None):
     if out_pattern is None: out_pattern = in_pattern
-    def wrapper( func ):
-        return NamedTransformImpl( name, in_pattern, out_pattern, func )
+    def wrapper(func):
+        return NamedTransformImpl(name, in_pattern, out_pattern, func)
     return wrapper
 
 Given = step_decorator('given')
